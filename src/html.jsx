1251922409b11d0b9b7f0997f167cfc3aff5723e
Timo Aho <zeukkari@gmail.com>
2018-06-03 08:13:38 +0300

    favicon

--- src/html.jsx
+++ src/html.jsx
@@ -2,7 +2,8 @@
 /* eslint import/extensions:"off" */
 /* eslint global-require:"off" */
 import React from "react";
-import favicon from "./favicon.png";
+
+const faviconPath = "/logos/favicon.ico";
 
 let inlinedStyles = "";
 if (process.env.NODE_ENV === "production") {
@@ -35,7 +36,7 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1.0"
           />
           {this.props.headComponents}
-          <link rel="shortcut icon" href={favicon} />
+          <link rel="shortcut icon" href={faviconPath} />
           {css}
         </head>
         <body>
